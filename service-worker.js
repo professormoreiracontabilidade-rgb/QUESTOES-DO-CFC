fec50525953778043c2a37188e3b18dfe3d3249d
professormoreiracontabilidade-rgb <professormoreira.contabilidade@gmail.com>
2026-08-01 21:42:33 -0300

    Nova versão do aplicativo com modo Quiz

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='questoes-cfc-quiz-v2';
+const CACHE='questoes-cfc-quiz-v3';
 const ASSETS=['./','index.html','style.css','app.js','manifest.json','logo.png','icones/icon-192.png','icones/icon-512.png',...Array.from({length:20},(_,i)=>`imagens/${String(i+1).padStart(2,'0')}.png`)];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
